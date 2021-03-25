6bd962dfefbf0e157930d6a724b898a8b4ca9218
Vivek Tadiparthi <saivivekreddy.s6@gmail.com>
2021-03-24 21:24:54 -0500

    updating user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var counter=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  counter++
+  res.send('User accesses are:'+counter);
 });
 
 module.exports = router;
