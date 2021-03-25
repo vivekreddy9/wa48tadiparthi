6e09b6fac6bf221d74f78d09064e6d0fbd850b02
Vivek Tadiparthi <saivivekreddy.s6@gmail.com>
2021-03-24 21:56:20 -0500

    new endpoint

---
+++ routes/computation.js
@@ -0,0 +1,15 @@
+var express=require('express')
+
+var router=express.Router();
+
+router.get('/',(req,res)=>{
+    let randomNum=Math.random()*100
+    let data=req.query.x
+    console.log(data)
+    res.render('compute',{
+        r:`sqrt applied to ${randomNum} is ${Math.sqrt(randomNum)}`,
+        q:` sqrt applied to ${data} is ${Math.sqrt(data)}`
+    })
+})
+
+module.exports=router
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var counter=0;
+var value=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   counter++
-  res.send('User accesses are:'+counter);
+  res.send('User accesses are:'+ value);
 });
 
 module.exports = router;
